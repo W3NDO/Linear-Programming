65d70f6bad79b79d0c9c226df60758e3b836ed67
W3NDO <wendonyang@gmail.com>
2020-05-22 10:48:00 +0300

    Had to start again
    Pivoting logic is harder than I thought, lol

--- pivot.rb
+++ pivot.rb
@@ -1,30 +1,53 @@
 ### Code to pivot linear equations ###
 # Definitely needs a GUI soon
-#Hope this works.
+#A couple steps to be followed#.
+=begin
+ 	1. Get the system of equations from the user
+ 		i) Dependent variables
+ 		ii) independent variables
+ 		iii) Coefficients
+
+ 	2. Get the pivot point. It is a coefficient
+
+ 	3. Do the pivot logic
+
+ 		 __		  __		 __				  	 ___
+ 		|	p 	r 	|		|	(1/p) 		(r/p)   |
+ 		|	 		| 	=>  |     				    |
+ 		|__	c	q __|       |__	(-c/p)  (q-(rc/p))__|
+
+ 	4. Return the pivoted result
+
+
+=end
+
 #W3NDO
 
-def pivot()
-	puts "Enter the number of dependent variables"
-	dependentVarCount = gets.to_i
-	puts "Enter the number of independent variables"
-	independentVarCount = gets.to_i
-	equationSet = []
-	dependentVarSet = []
-	if dependentVarCount == 1
-		puts "Enter the coefficients of the first equation. Note that the variables with a coefficient of 0 must be entered as 0"
-		temp = gets.chomp
-		equationSet << temp.scan(/-?\w+/)[0..temp.scan(/\w+/).length-2] #regex to get the independent variables
-		dependentVarSet << temp.scan(/-?\w+/).last #regex to get the dependent variable
-	else
-		for i in 1..dependentVarCount
-			puts "Enter the coefficients of equation #{i}. Note that variables with a coefficient of 0 must be entered as 0"
-			temp = gets.chomp
-			equationSet << temp.scan(/-?\d+/)[0..temp.scan(/\d+/).length-2]
-			#dependentVarSet << temp.scan(/-?\d+/).last
-		end
-	end
-	puts "#{dependentVarSet}"
-	puts "#{equationSet}"
-
+#step 1: Get the System of equation sfrom the user
+puts "Enter the number of dependant variables and the letter denoting it eg (3,s)"
+x=gets
+dependentVarCount = x.scan(/[0-9]+/).join().to_i
+dependentVarSymbol =  x.scan(/[a-z A-Z]/).join()
+puts "Enter the number of independent variables the letter denoting it eg (2,x)"
+y = gets
+independentVarCount = y.scan(/[0-9]+/).join().to_i
+independentVarSymbol =  y.scan(/[a-z A-Z]/).join()
+coefficientArr = []
+independentVarSet = []
+dependentVarSet =[]
+for i in 1..independentVarCount
+	independentVarSet << independentVarSymbol+i.to_s
 end
-pivot()
+for i in 1..dependentVarCount
+	dependentVarSet << dependentVarSymbol+i.to_s
+end
+for i in 1..dependentVarCount
+	puts "Enter the coefficients of equation #{i}. for instance, if the equation is 2x1+3x3 =s1, enter (2,3) without the brackets"
+	coeffSet = gets.chomp.split(',')
+	coefficientArr << coeffSet
+end
+
+
+puts "The Dependant variables are: #{dependentVarSet}"
+puts "The Independant variables are: #{independentVarSet}"
+puts "The Coefficients are: #{coefficientArr}}"
